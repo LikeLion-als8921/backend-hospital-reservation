f2c5a2c1ff1f9a90b6ab00a3e886ee3b124860d2
als8921 <als8921@naver.com>
2025-03-29 23:22:50 +0900

    feat : 필드 Getter 추가

--- src/main/java/com/example/hospitalreservation/model/Reservation.java
+++ src/main/java/com/example/hospitalreservation/model/Reservation.java
@@ -10,11 +10,29 @@ public class Reservation {
 
     // TODO : 필요한 메서드가 있다면 작성해주세요.
 
-    public Long getId() {
-        return id;
+    public Reservation(Long doctorId, Long patientId, LocalDateTime reservationTime) {
+        this.doctorId = doctorId;
+        this.patientId = patientId;
+        this.reservationTime = reservationTime;
     }
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getDoctorId() {
+        return doctorId;
+    }
+
+    public Long getPatientId() {
+        return patientId;
+    }
+
+    public LocalDateTime getReservationTime() {
+        return reservationTime;
     }
 }
