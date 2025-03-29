e887d3b19aafa13e397dc2dc853d625c1e9fc7c9
als8921 <als8921@naver.com>
2025-03-30 00:40:35 +0900

    feat : Controller 어노테이션 및 리턴값 구현

--- src/main/java/com/example/hospitalreservation/controller/ReservationController.java
+++ src/main/java/com/example/hospitalreservation/controller/ReservationController.java
@@ -1,35 +1,53 @@
 package com.example.hospitalreservation.controller;
 
+import com.example.hospitalreservation.model.Reservation;
+import com.example.hospitalreservation.repository.ReservationRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+
 // TODO : 컨트롤러에 필요한 어노테이션을 작성해주세요.
 // TODO : 요청 경로는 templates를 참고하여 작성해주세요.
+@Slf4j
+@Controller
+@RequestMapping("/reservations")
 public class ReservationController {
 
     // TODO : 주입 받아야 할 객체를 설정해주세요.
+    private ReservationRepository reservationRepository = new ReservationRepository();
 
     // TODO : 필요한 어노테이션을 작성해주세요.
+    @GetMapping
     public String getReservations(Model model) {
         // TODO : 예약 메인 페이지를 가져오는 코드를 작성해주세요.
-        return null;
+        model.addAttribute("reservations", reservationRepository.findAll());
+        return "index";
     }
 
     // TODO : 필요한 어노테이션을 작성해주세요.
+    @GetMapping("/new")
     public String showReservationForm() {
         // TODO : 예약하기 페이지를 가져오는 코드를 작성해주세요.
-        return null;
+        return "reservation_form";
     }
 
     // TODO : 필요한 어노테이션을 작성해주세요.
+    @PostMapping
     public String createReservation(@RequestParam Long doctorId, @RequestParam Long patientId) {
         // TODO : 예약을 진행하는 코드를 작성해주세요.
-        return null;
+        Reservation reservation = new Reservation(doctorId, patientId, LocalDateTime.now());
+        reservationRepository.save(reservation);
+        return "redirect:/reservations";
     }
 
+    @PostMapping("/delete/{id}")
     // TODO : 필요한 어노테이션을 작성해주세요.
     public String cancelReservation(@PathVariable Long id) {
         // TODO : 예약을 취소하는 코드를 작성해주세요.
-        return null;
+        reservationRepository.deleteById(id);
+        return "redirect:/reservations";
     }
 }
