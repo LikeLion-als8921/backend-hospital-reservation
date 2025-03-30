53f5c605b09cf724961a3249efffdc1269e525d0
als8921 <als8921@naver.com>
2025-03-30 21:42:11 +0900

    chore : 존재하지 않는 에러 메세지 추가

--- src/main/java/com/example/hospitalreservation/model/ReservationException.java
+++ src/main/java/com/example/hospitalreservation/model/ReservationException.java
@@ -3,7 +3,8 @@ package com.example.hospitalreservation.model;
 public enum ReservationException
 {
     NOT_AVAILABLE_TIME("의사의 진료 가능 시간 (09:00~17:00) 내에서만 예약할 수 있습니다."),
-    RESERVATION_TIME_ALREADY_EXISTS("해당 시간에는 이미 예약이 있습니다. 다른 시간을 선택해주세요.");
+    RESERVATION_TIME_ALREADY_EXISTS("해당 시간에는 이미 예약이 있습니다. 다른 시간을 선택해주세요."),
+    NOT_EXIST_RESERVATION("존재하지 않는 예약입니다.");
 
     private final String errorMessage;
 
