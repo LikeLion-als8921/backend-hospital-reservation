240e8253e603c42044d14b73970dc183c70d9bc2
als8921 <als8921@naver.com>
2025-04-05 19:42:55 +0900

    chore : 필요없는 import 제거

--- src/main/java/com/example/hospitalreservation/model/Reservation.java
+++ src/main/java/com/example/hospitalreservation/model/Reservation.java
@@ -1,5 +1,7 @@
 package com.example.hospitalreservation.model;
 
+import com.example.hospitalreservation.dto.CreateReservationRequest;
+
 import java.time.LocalDateTime;
 
 public class Reservation {
