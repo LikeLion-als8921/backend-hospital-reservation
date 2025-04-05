b07f7759d79fa3ecfa793be3b904f5a1079bc263
als8921 <als8921@naver.com>
2025-04-05 15:22:57 +0900

    chore : 페이지 전환 부분 필요없는 코드 제거

--- src/main/java/com/example/hospitalreservation/controller/ReservationController.java
+++ src/main/java/com/example/hospitalreservation/controller/ReservationController.java
@@ -1,55 +1,19 @@
 package com.example.hospitalreservation.controller;
 
-import com.example.hospitalreservation.model.Reservation;
-import com.example.hospitalreservation.service.ReservationService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDateTime;
 
 @Slf4j
 @Controller
 @RequestMapping("/reservations")
 public class ReservationController {
-
-    private final ReservationService reservationService;
-    public ReservationController(ReservationService reservationService) {
-        this.reservationService = reservationService;
-    }
-
     @GetMapping
-    public String getReservations(Model model) {
-        model.addAttribute("reservations", reservationService.getAllReservations());
-        return "index";
-    }
+    public String getReservations() { return "index"; }
 
     @GetMapping("/new")
     public String showReservationForm() {
         return "reservation_form";
     }
 
-    @PostMapping
-    public String createReservation(@RequestParam Long doctorId, @RequestParam Long patientId, @RequestParam LocalDateTime reservationTime) {
-        Reservation reservation = new Reservation(doctorId, patientId, reservationTime);
-        try {
-            reservationService.createReservation(reservation);
-        }
-        catch (Exception e) {
-            log.error(e.getMessage());
-        }
-        return "redirect:/reservations";
-    }
-
-    @PostMapping("/delete/{id}")
-    public String cancelReservation(@PathVariable Long id) {
-        try {
-            reservationService.cancelReservation(id);
-        }
-        catch (Exception e) {
-            log.error(e.getMessage());
-        }
-        return "redirect:/reservations";
-    }
 }
