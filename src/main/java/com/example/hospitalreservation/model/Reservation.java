e8ef64b7308abaac4d9105967c841480852505ec
als8921 <als8921@naver.com>
2025-04-05 16:02:13 +0900

    feat : Reservation 클래스 필드 수정

--- src/main/java/com/example/hospitalreservation/model/Reservation.java
+++ src/main/java/com/example/hospitalreservation/model/Reservation.java
@@ -6,37 +6,30 @@ public class Reservation {
     private Long id;
     private Long doctorId;
     private Long patientId;
-    private LocalDateTime reservationTime;
+    private LocalDateTime reservationStartTime;
+    private LocalDateTime reservationEndTime;
+    private String reason;
+    private Long fee;
 
     // 필요한 메서드가 있다면 작성해주세요.
 
-    public Reservation(Long doctorId, Long patientId, LocalDateTime reservationTime) {
+    public Reservation(Long doctorId, Long patientId, LocalDateTime reservationStartTime, LocalDateTime reservationEndTime, String reason) {
         this.doctorId = doctorId;
         this.patientId = patientId;
-        this.reservationTime = reservationTime;
+        this.reservationStartTime = reservationStartTime;
+        this.reservationEndTime = reservationEndTime;
+        this.reason = reason;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    public void setId(Long id) {this.id = id;}
+    public void setFee(Long fee) {this.fee = fee;}
 
-    public Long getId() {
-        return id;
-    }
+    public Long getId() {return id;}
+    public Long getDoctorId() {return doctorId;}
+    public Long getPatientId() {return patientId;}
+    public LocalDateTime getReservationStartTime() {return reservationStartTime;}
+    public LocalDateTime getReservationEndTime() {return reservationEndTime;}
+    public String getReason() {return reason;}
+    public Long getFee() {return fee;}
 
-    public Long getDoctorId() {
-        return doctorId;
-    }
-
-    public Long getPatientId() {
-        return patientId;
-    }
-
-    public LocalDateTime getReservationTime() {
-        return reservationTime;
-    }
-
-    public int getHour(){
-        return reservationTime.getHour();
-    }
 }
