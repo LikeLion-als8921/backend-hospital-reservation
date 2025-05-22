296d56912666820fdb0d77a593dd8ef451e6cd1a
als8921 <als8921@naver.com>
2025-05-22 19:26:23 +0900

    fix : password 저장 안되는 오류 해결

--- src/main/java/com/example/hospitalreservation/model/User.java
+++ src/main/java/com/example/hospitalreservation/model/User.java
@@ -15,7 +15,7 @@ public class User {
     private String password;
 
     public User() {}
-    public User(String username, String encodedPassword) {
+    public User(String username, String password) {
         this.username = username;
         this.password = password;
     }
