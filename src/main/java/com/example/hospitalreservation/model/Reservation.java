ba5e781087e7f7cb7588e000e055f309e2cccfaa
als8921 <als8921@naver.com>
2025-05-22 13:00:02 +0900

    feat : Reservation에서 doctor, patient를 다대일로 사용

--- src/main/java/com/example/hospitalreservation/model/Reservation.java
+++ src/main/java/com/example/hospitalreservation/model/Reservation.java
@@ -1,27 +1,39 @@
 package com.example.hospitalreservation.model;
 
-import java.time.LocalDateTime;
+import jakarta.persistence.*;
 
+import java.time.LocalDateTime;
+@Entity
 public class Reservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Long doctorId;
-    private Long patientId;
     private LocalDateTime reservationStartTime;
     private LocalDateTime reservationEndTime;
     private String reason;
     private Long fee;
 
-    public Reservation(Long doctorId, Long patientId, LocalDateTime reservationStartTime, LocalDateTime reservationEndTime, String reason) {
-        this.doctorId = doctorId;
-        this.patientId = patientId;
+    @ManyToOne @JoinColumn(name = "patient_id")
+    private Patient patient;
+
+    @ManyToOne @JoinColumn(name = "doctor_id")
+    private Doctor doctor;
+
+    public Reservation() {
+
+    }
+
+    public Reservation(Doctor doctor, Patient patient, LocalDateTime reservationStartTime, LocalDateTime reservationEndTime, String reason) {
+        this.doctor = doctor;
+        this.patient = patient;
         this.reservationStartTime = reservationStartTime;
         this.reservationEndTime = reservationEndTime;
         this.reason = reason;
     }
 
     public Long getId() {return id;}
-    public Long getDoctorId() {return doctorId;}
-    public Long getPatientId() {return patientId;}
+    public Long getDoctorId() {return doctor.getId();}
+    public Long getPatientId() {return patient.getId();}
     public LocalDateTime getReservationStartTime() {return reservationStartTime;}
     public LocalDateTime getReservationEndTime() {return reservationEndTime;}
     public String getReason() {return reason;}
